cf14e88ae9609d3fc811ee8e5a6c34176722d64b
Tanmay-Borde <tanmayborde64@gmail.com>
2024-07-09 12:48:33 +0530

    Projects Tab completed. New images added.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,77 +6,83 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import { Avatar, Chip, Container, Stack } from '@mui/material';
 import { isMobile } from 'react-device-detect';
+import Link from '@mui/material/Link';
+
+const projects = [
+    {
+        projectId: 'pid1',
+        projectTitle: 'Polarized Opinion Viewer using ML.',
+        projectBrief: 'Combats echo chambers. Analyzes polarizng sentiment across sources to empower informed decisions on any topic.',
+        skills: ['Python', 'REST-API', 'Machine-Learning']
+    },
+    {
+        projectId: 'pid2',
+        projectTitle: 'Developers Sandbox.',
+        projectBrief: 'All-in-one kickstarter wesite for beginner developers. Choose your language & get everything you need to code in minutes - tools, environments, step-by-step guides.',
+        skills: ['HTML', 'CSS', 'Bootstrap', 'JSP']
+    },
+    {
+        projectId: 'pid3',
+        projectTitle: 'ML Algorithm Recommender.',
+        projectBrief: 'Guides beginners to choose optimal Machine Learning algorithm for their datasets.',
+        skills: ['Python', 'Machine-Learning']
+    },
+    {
+        projectId: 'pid4',
+        projectTitle: 'Consumer Behaviour Analysis.',
+        projectBrief: 'R programming mini project developed for consumer behaviour analysis for pre and post product development phase.',
+        skills: ['R-Programming']
+    },
+    {
+        projectId: 'pid5',
+        projectTitle: 'Carbon Footprint Calculator.',
+        projectBrief: 'The software is developed for the Census Department of India with the sole purpose of effective nationwide outreach regarding the ecological issues and collect data.',
+        skills: ['Java']
+    },
+    {
+        projectId: 'pid6',
+        projectTitle: 'SPA Website for a Restaurant.',
+        projectBrief: 'A Single Page Application website for a Restaurant. The website utilizes JSON-Server for dynamic data retrieval and storage, ensuring a seamless user experience.',
+        skills: ['Angular', 'JSON-Server']
+    },
+]
 
 export default function Projects() {
     console.log(isMobile ? 'Mobile device' : 'Desktop');
     return (
         <>
-            <Container sx={{ display: 'flex' }}>
+            <Container sx={{ display: 'flex', padding: 2 }}>
                 <Box sx={{ flexGrow: 1 }}>
-                    <Grid container spacing={2} direction={isMobile ? 'column' : 'row'} flexWrap={'wrap'} >
-                        <Grid item xs={6}>
-                            <Card sx={{ minHeight: 200, flexGrow: 1 }} >
-                                <CardContent>
-                                    <Typography variant="h5" component="div">
-                                        Polarized Opinion Viewer using ML
-                                    </Typography>
-                                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        {`Combats echo chambers. Analyzes polarizng sentiment across sources to empower informed decisions on any topic.`}
-                                    </Typography>
-                                    <Stack spacing={2} padding={2} direction={isMobile ? 'column' : 'row'} flexGrow={1} marginBottom="auto" >
-                                        <Grid item xs={12} >
-                                            <Chip avatar={<Avatar src={`${process.env.PUBLIC_URL}/content/images/Python.png`} />} label="Python" />
-                                            <Chip avatar={<Avatar src={`${process.env.PUBLIC_URL}/content/images/Java.png`} />} label="JAVA" />
+                    <Grid container spacing={1} direction={isMobile ? 'column' : 'row'} flexWrap={'wrap'} >
+                        {projects.map((project, index) => (
+                            <Grid item xs={6} spacing={2}>
+                                <Card key={index} sx={{ minHeight: 240, flexGrow: 1 }} >
+                                    <CardContent>
+                                        <Typography variant="h5" component="div">
+                                            {project.projectTitle}
+                                        </Typography>
+                                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                        </Typography>
+                                        <Typography variant="body2">
+                                            {project.projectBrief}
+                                        </Typography>
+                                        <Grid justifyContent={'flex-end'}>
+                                            <Stack spacing={1} padding={2} direction={isMobile ? 'column' : 'row'} flexGrow={1} marginBottom="auto" >
+                                                {project.skills.map((skill, index) => (
+                                                    <Grid item direction={isMobile ? 'column' : 'row'}>
+                                                        <Chip avatar={<Avatar src={`${process.env.PUBLIC_URL}/content/images/${skill}.png`} />} label={skill} />
+                                                    </Grid>
+                                                ))}
+                                            </Stack>
                                         </Grid>
-                                    </Stack>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Card sx={{ minHeight: 200 }}>
-                                <CardContent>
-                                    <Typography variant="h5" component="div">
-                                        Developer's Sandbox
-                                    </Typography>
-                                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        {`All-in-one kickstarter wesite for beginner developers. Choose your language & get everything you need to code in minutes - tools, environments, step-by-step guides.`}
-                                    </Typography>
-
-                                    <Stack padding={2} direction={isMobile ? 'column' : 'row'} flexDirection={isMobile ? 'column' : 'row'} spacing={2}>
-                                        <Grid spacing={2} item xs={12} >
-                                            <Chip avatar={<Avatar src={`${process.env.PUBLIC_URL}/content/images/HTML.png`} />} label="HTML" />
-                                            <Chip avatar={<Avatar src={`${process.env.PUBLIC_URL}/content/images/Java.png`} />} label="JAVA" />
-                                        </Grid>
-                                    </Stack>
-
-                                </CardContent>
-                            </Card>
-                            <Card sx={{ minHeight: 200, flexGrow: 1, marginTop: 2 }} >
-                                <CardContent>
-                                    <Typography variant="h5" component="div">
-                                        {`ML Algorithm Recommender`}
-                                    </Typography>
-                                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                    </Typography>
-                                    <Typography variant="body2">
-                                        {`Guides beginners to choose optimal algorithms for their datasets.`}
-                                    </Typography>
-                                    <Stack spacing={2} padding={2} direction={isMobile ? 'column' : 'row'} flexGrow={1} marginBottom="auto" >
-                                        <Grid item xs={12} >
-                                            <Chip avatar={<Avatar src={`${process.env.PUBLIC_URL}/content/images/Python.png`} />} label="Python" />
-                                            <Chip avatar={<Avatar src={`${process.env.PUBLIC_URL}/content/images/Java.png`} />} label="JAVA" />
-                                        </Grid>
-                                    </Stack>
-                                </CardContent>
-                            </Card>
-                        </Grid>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        ))}
                     </Grid>
                 </Box>
             </Container >
+            <Link href="https://github.com/Tanmay-Borde?tab=repositories" underline="none" target="_blank">View more projects...</Link>
         </>
     );
 }
